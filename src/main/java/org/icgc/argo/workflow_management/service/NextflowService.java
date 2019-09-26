a1b37f6f2395c47405f6494990f3529a9fd22c0f
Alex <lepsalex@gmail.com>
2019-09-26 10:14:23 -0400

    skeleton nextflow service

---
+++ src/main/java/org/icgc/argo/workflow_management/service/NextflowService.java
@@ -0,0 +1,15 @@
+package org.icgc.argo.workflow_management.service;
+
+public class NextflowService implements WorkflowExecutionService {
+    public String run(WESRunConfig params) {
+        return "Not implemented!";
+    }
+
+    public String cancel(String runId) {
+        return "Not implemented!";
+    }
+
+    public String getServiceInfo() {
+        return "Not implemented!";
+    }
+}
