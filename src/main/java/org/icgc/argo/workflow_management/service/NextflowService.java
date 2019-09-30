ba3d8133684ed5119a610e0b45de4cb3809444db
Alex Lepsa <lepsalex@gmail.com>
2019-09-30 14:40:53 -0400

    rough flex but it's working

--- src/main/java/org/icgc/argo/workflow_management/service/NextflowService.java
+++ src/main/java/org/icgc/argo/workflow_management/service/NextflowService.java
@@ -1,17 +1,24 @@
 package org.icgc.argo.workflow_management.service;
 
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import nextflow.cli.CliOptions;
 import nextflow.cli.CmdKubeRun;
 import nextflow.cli.Launcher;
+import nextflow.k8s.K8sDriverLauncher;
 import org.icgc.argo.workflow_management.controller.model.RunsResponse;
 import org.icgc.argo.workflow_management.service.model.WESRunParams;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ReflectionUtils;
 import reactor.core.publisher.Mono;
 
-import java.util.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 import static java.util.Objects.nonNull;
 
@@ -21,38 +28,13 @@ public class NextflowService implements WorkflowExecutionService {
   public Mono<RunsResponse> run(WESRunParams params) {
     return Mono.create(
         callback -> {
-
-          // Construct cmd params from workflow params and url
-          val cmdParams = new HashMap<String, Object>();
-
-          cmdParams.put("args", Arrays.asList(params.getWorkflow_url()));
-          cmdParams.put("params", params.getWorkflow_params());
-
-          // Add static options (todo: make into config?)
-          val processOptions = new HashMap<String, String>();
-          processOptions.put("container", "quay.io/pancancer/pcawg-bwa-mem:latest");
-
-          cmdParams.put("process", processOptions);
-          cmdParams.put("namespace", "nextflow-pcawg");
-          cmdParams.put("latest", true);
-          cmdParams.put("volMounts", Arrays.asList("nextflow-pv-claim:/mnt/volume/nextflow"));
-
-          // Add a launcher to the mix
-          val launcherParams = new HashMap<String, Object>();
-          val cliOptions = new CliOptions();
-          cliOptions.setBackground(true);
-          launcherParams.put("options", cliOptions);
-
-          val launcher = createWithReflection(Launcher.class, launcherParams);
-          cmdParams.put("launcher", launcher.orElse(null));
-
-          // Using reflection (because groovy) init a CmdKubeRun
-          val cmd = createWithReflection(CmdKubeRun.class, cmdParams);
+          val cmd = createCmd(createLauncher(), params);
+          val driver = createDriver(cmd);
 
           // Run it!
-          cmd.ifPresent(CmdKubeRun::run);
-
-          val response = new RunsResponse("test output");
+          driver.run(params.getWorkflow_url(), Arrays.asList());
+          val exitStatus = driver.shutdown();
+          val response = new RunsResponse(exitStatus == 0 ? cmd.getRunName() : "Error!");
 
           try {
             callback.success(response);
@@ -70,6 +52,59 @@ public class NextflowService implements WorkflowExecutionService {
     return null;
   }
 
+  private Launcher createLauncher() {
+    // Add a launcher to the mix
+    val launcherParams = new HashMap<String, Object>();
+    val cliOptions = new CliOptions();
+    cliOptions.setBackground(true);
+    launcherParams.put("options", cliOptions);
+
+    return createWithReflection(Launcher.class, launcherParams).orElseThrow();
+  }
+
+  private CmdKubeRun createCmd(@NonNull Launcher launcher, @NonNull WESRunParams params) {
+    // Construct cmd params from workflow params and url
+    val cmdParams = new HashMap<String, Object>();
+
+    cmdParams.put("args", Arrays.asList(params.getWorkflow_url()));
+    cmdParams.put("params", params.getWorkflow_params());
+
+    // Add static options (todo: make into config?)
+    val processOptions = new HashMap<String, String>();
+    processOptions.put("container", "quay.io/pancancer/pcawg-bwa-mem:latest");
+
+    cmdParams.put("process", processOptions);
+    cmdParams.put("namespace", "nextflow-pcawg");
+    cmdParams.put("latest", true);
+    cmdParams.put("volMounts", Arrays.asList("nextflow-pv-claim:/mnt/volume/nextflow"));
+    cmdParams.put("launcher", launcher);
+
+    return createWithReflection(CmdKubeRun.class, cmdParams).orElseThrow();
+  }
+
+  private K8sDriverLauncher createDriver(@NonNull CmdKubeRun cmd) {
+    Method checkRunName = null;
+    try {
+      checkRunName = CmdKubeRun.class.getDeclaredMethod("checkRunName");
+    } catch (NoSuchMethodException e) {
+      e.printStackTrace();
+    }
+    assert checkRunName != null;
+    checkRunName.setAccessible(true);
+    try {
+      checkRunName.invoke(cmd);
+    } catch (IllegalAccessException | InvocationTargetException e) {
+      e.printStackTrace();
+    }
+
+    val k8sDriverLauncherParams = new HashMap<String, Object>();
+    k8sDriverLauncherParams.put("cmd", cmd);
+    k8sDriverLauncherParams.put("runName", cmd.getRunName());
+    k8sDriverLauncherParams.put("background", true);
+
+    return createWithReflection(K8sDriverLauncher.class, k8sDriverLauncherParams).orElseThrow();
+  }
+
   private <T> Optional<T> createWithReflection(Class<T> objClass, Map<String, Object> params) {
     try {
       T obj = objClass.newInstance();
@@ -82,14 +117,15 @@ public class NextflowService implements WorkflowExecutionService {
   }
 
   private <T> T reflectionFactory(Class<T> objClass, T obj, Map<String, Object> map) {
-    map.forEach((key, value) -> {
-      val field = ReflectionUtils.findField(objClass, key);
+    map.forEach(
+        (key, value) -> {
+          val field = ReflectionUtils.findField(objClass, key);
 
-      if (nonNull(field)) {
-        ReflectionUtils.makeAccessible(field);
-        ReflectionUtils.setField(field, obj, value);
-      }
-    });
+          if (nonNull(field)) {
+            ReflectionUtils.makeAccessible(field);
+            ReflectionUtils.setField(field, obj, value);
+          }
+        });
 
     return obj;
   }
