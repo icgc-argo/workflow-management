daa45d6dd31fb601920ef58dbd87f11ca40b0f17
Alex Lepsa <lepsalex@gmail.com>
2019-12-04 14:30:04 -0500

    more fmt

--- src/main/java/org/icgc/argo/workflow_management/controller/model/RunResponse.java
+++ src/main/java/org/icgc/argo/workflow_management/controller/model/RunResponse.java
@@ -2,9 +2,8 @@ package org.icgc.argo.workflow_management.controller.model;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import javax.validation.constraints.NotNull;
-
 import io.swagger.annotations.ApiModel;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,7 +12,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
-@ApiModel(description = "A successful run will will return the runId which can be used to get info about that run")
+@ApiModel(
+    description =
+        "A successful run will will return the runId which can be used to get info about that run")
 public class RunResponse {
   @NotNull private String runId;
 }
