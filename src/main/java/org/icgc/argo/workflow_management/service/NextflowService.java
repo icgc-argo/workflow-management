47790f0d3fc0d69b0749cb435ac9441259b4fb0c
Alex <lepsalex@gmail.com>
2020-09-17 10:36:28 -0400

    hotfix: typo for cancel pod not found

--- src/main/java/org/icgc/argo/workflow_management/service/NextflowService.java
+++ src/main/java/org/icgc/argo/workflow_management/service/NextflowService.java
@@ -220,7 +220,7 @@ public class NextflowService implements WorkflowExecutionService {
             .orElseThrow(
                 () ->
                     new RuntimeException(
-                        format("Cannot found executor pod with runId: %s.", runId)));
+                        format("Cannot find executor pod with runId: %s.", runId)));
     return valueOf(executorPod.getStatus().getPhase().toUpperCase());
   }
 
