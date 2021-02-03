c55003567963000f75e09fb21e93b2db5735f282
jaserud <udjaser@gmail.com>
2021-02-03 09:44:29 -0500

    add todo comment for @NonNull removal

--- src/main/java/org/icgc/argo/workflow_management/service/wes/model/WfManagementEvent.java
+++ src/main/java/org/icgc/argo/workflow_management/service/wes/model/WfManagementEvent.java
@@ -33,6 +33,7 @@ public class WfManagementEvent {
   @NonNull private String runId;
   @NonNull private String event;
   @NonNull private String utcTime;
+  // TODO - workflowUrl needs to be @NonNull, its missing it now because currently only INITIALIZING events have this info available
   private String workflowUrl;
   private String workflowType;
   private String workflowTypeVersion;
